7d2cd1965ba773bca8c8ec66b3b25ae3cf6d642a
Ryan Wynn <ryan.james.wynn@gmail.com>
2026-08-10 16:09:00 +0400

    Adjusted the z-index of the particles background canvas to ensure it stays behind other elements on the page.

--- SmartBrain/src/Components/ImageLinkForm/ImageLinkForm.jsx
+++ SmartBrain/src/Components/ImageLinkForm/ImageLinkForm.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const ImageLinkForm = () => {
   return (
     <div className="image-link-form">
-      <p className="text-md-center image-link-copy">
+      <p className="image-link-copy">
         {'This magic brain will detect faces in your pictures. Give it a try.'}
       </p>
       <div className="image-link-box">
         <div className="image-link-row">
           <input className="image-link-input" type="text" />
-          <button className="btn btn-primary btn-lg image-link-button">Detect</button>
+          <button className="btn image-link-button">Detect</button>
         </div>
       </div>
     </div>
--- SmartBrain/src/Components/Navigation/Navigation.jsx
+++ SmartBrain/src/Components/Navigation/Navigation.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Navigation = () => {
   return (
     <nav className="navigation">
-      <a className="btn btn-primary btn-lg" href="#" role="button">
+      <a className="btn" href="#" role="button">
         Sign Out
       </a>
     </nav>
