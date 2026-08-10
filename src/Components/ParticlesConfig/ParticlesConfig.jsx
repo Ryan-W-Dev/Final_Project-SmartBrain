452e549cdee9be100c6fe0ee3f8e8937001819a4
Ryan Wynn <ryan.james.wynn@gmail.com>
2026-08-10 21:31:45 +0400

    Fixes

--- src/Components/ParticlesConfig/ParticlesConfig.jsx
+++ src/Components/ParticlesConfig/ParticlesConfig.jsx
@@ -3,12 +3,16 @@ const isSmallPhone = typeof window !== 'undefined' && window.innerWidth <= 430;
 export const particleConfig = {
   num: isSmallPhone ? [0, 1] : [4, 6],
   rps: isSmallPhone ? 0.05 : 0.3,
+  v: isSmallPhone ? 0.1 : 0.3,
   radius: [2, 4],
   life: [1, 2],
-  v: [0.001, 0.003],
+  v: 0.1,
   tha: [-180, 180],
   scale: [1, 2],
   position: 'all',
   cross: 'dead',
   random: 8,
+  zIndex: -1,
+  top: 0,
+  left: 0,
 };
